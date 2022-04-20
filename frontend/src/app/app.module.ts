bfed6877b2f56d5717854b45b52db02e909535b8
Md. Inzamam-Ul Haque Sobuz <48618774+inzamam-inz@users.noreply.github.com>
2022-04-20 17:06:50 +0600

    All component create and login done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewModelComponent } from './view-model/view-model.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewModelComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    NavbarComponent,
+    NewsfeedComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
